c9f1279e81fb23820fb8df463ff8734e8a07fca8
jskako <jskako@gmail.com>
2020-05-16 02:14:35 +0200

    Added new methods to Bitcoin class

--- app/src/main/java/com/example/hdwall/backend/Bitcoin.java
+++ app/src/main/java/com/example/hdwall/backend/Bitcoin.java
@@ -1,5 +1,10 @@
 package com.example.hdwall.backend;
 
+import org.bitcoinj.crypto.ChildNumber;
+import org.bitcoinj.crypto.DeterministicHierarchy;
+import org.bitcoinj.crypto.DeterministicKey;
+import org.bitcoinj.crypto.HDKeyDerivation;
+import org.bitcoinj.crypto.HDUtils;
 import org.bitcoinj.crypto.MnemonicCode;
 
 import java.security.SecureRandom;
@@ -9,9 +14,7 @@ import static java.lang.String.join;
 
 public class Bitcoin {
 
-    public void bitcoin(){
-
-    }
+    private final String bip44Path = "44H/0H/0H";
 
     //Entropy of 128Bits
     public byte[] entropyGenerator(){
@@ -34,5 +37,25 @@ public class Bitcoin {
         return words;
     }
 
+    //Seed generator
+    public byte[] seedGenerator(List<String> words){
+        String passphrase = "";
+        byte seed[] = MnemonicCode.toSeed(words, passphrase);
+        return seed;
+    }
+
+    //Generate master key
+    public DeterministicKey generateMasterKey(byte seed[]){
+        DeterministicKey masterKey = HDKeyDerivation.createMasterPrivateKey(seed);
+        return masterKey;
+    }
+
+    //Derive bitcoin root key
+    public DeterministicKey deriveBitcoinRootKey(DeterministicKey masterKey){
+        List<ChildNumber> keyPath = HDUtils.parsePath(bip44Path);
+        DeterministicHierarchy hierarchy = new DeterministicHierarchy(masterKey);
+        DeterministicKey walletKey = hierarchy.get(keyPath, false, true);
+        return walletKey;
+    }
 }
 
